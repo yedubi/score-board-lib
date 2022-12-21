60d85292f35858ddc5f1f6c0bf398ff2bd73063c
yevhenii <dubinin.zvit@gmail.com>
2022-12-21 12:25:52 +0200

    add javadoc for LiveFootballWorldCupScoreBoard and FootballScoreBoard

--- lib/src/main/java/com/sportradar/scoreboard/FootballScoreBoard.java
+++ lib/src/main/java/com/sportradar/scoreboard/FootballScoreBoard.java
@@ -4,14 +4,46 @@ import com.sportradar.scoreboard.model.Game;
 
 import java.util.List;
 
+
+/**
+ * Interface for the FootballScoreBoard.
+ *
+ * @author Yevhenii Dubinin
+ */
+
 public interface FootballScoreBoard {
 
+    /**
+     * Create new game and add to the score board repository.
+     *
+     * @param homeTeam - home team name.
+     * @param awayTeam - away team name.
+     * @return {@code FootballGame}
+     */
     Game startGame(String homeTeam, String awayTeam);
 
+    /**
+     * Finish football by removing from score board repository.
+     *
+     * @param game - finished {@code FootballGame}.
+     * @throws FootballGameNotStartedException if try to finish not started game
+     */
     void finishGame(Game game) throws FootballGameNotStartedException;
 
+    /**
+     * Update football game score and save in repository.
+     *
+     * @param game   - {@code FootballGame} for update
+     * @param scores - pair of new scores.
+     * @throws FootballGameNotStartedException if try to update not started game
+     */
     void updateScore(Game game, int[] scores) throws FootballGameNotStartedException;
 
+    /**
+     * Returns the list of currently ongoing matches.
+     *
+     * @return A list of {@code FootballGame}
+     */
     List<Game> getSummary();
 
 }
--- lib/src/main/java/com/sportradar/scoreboard/impl/LiveFootballWorldCupScoreBoard.java
+++ lib/src/main/java/com/sportradar/scoreboard/impl/LiveFootballWorldCupScoreBoard.java
@@ -5,12 +5,26 @@ import com.sportradar.scoreboard.repository.FootballScoreBoardRepository;
 
 import java.util.Comparator;
 
+/**
+ * LiveFootballWorldCupScoreBoard extends AbstractFootballScoreBoard which provide implementation for
+ * FootballScoreBoard interface.
+ * And provide ScoreBoard of the football games sorted by their total score then by most recently started match
+ *
+ * @author Yevhenii Dubinin
+ */
+
 public class LiveFootballWorldCupScoreBoard extends AbstractFootballScoreBoard {
 
     public LiveFootballWorldCupScoreBoard(FootballScoreBoardRepository repository) {
         super(repository);
     }
 
+    /**
+     * Implements abstract getOrderingComparator comparator method
+     * Returns comparator for sorting football games by their total score then by most recently started match.
+     *
+     * @return {@code Comparator<FootballGame>} FootballGame comparator.
+     */
     protected Comparator<Game> getOrderingComparator() {
         return Comparator
                 .comparing(Game::getTotalScore)
